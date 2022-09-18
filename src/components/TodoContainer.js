af7f8ade849d39906863080df003dab28ca89a99
mule <belmeetmule@gmail.com>
2022-09-18 21:25:04 +0300

    add the todocontainer file with sample data via its state

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -22,10 +22,14 @@ class TodoContainer extends React.Component{
     }
     render(){
         return (
-        <>
-            <h1>Hello from Create React App</h1>
-            <p>I am in a React Component!</p>
-        </>
+        <div>
+            <h1>To do s</h1>
+            <ul>
+                {this.state.todos.map(item =>(
+                    <li> {item.title} </li>
+                ))}
+            </ul>
+        </div>
         )
     }
 }
