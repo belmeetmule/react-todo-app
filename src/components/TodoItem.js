44e6a2cad9b27806eea22390f08e1dd1d8107e2d
mule <belmeetmule@gmail.com>
2022-09-19 23:08:03 +0300

    add local storage feature to the ap

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -20,6 +20,10 @@ class TodoItem extends React.Component{
         console.log(event.key);
       }
 
+      componentWillUnmount() {
+        console.log("Cleaning up...")
+      }
+
     render(){
         const completedStyle = {
             fontStyle: "italic",
